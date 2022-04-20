ff21f01f0e9ae9aace92edf0a690a88d8f79b513
shethmanan <shethmanan1997@gmail.com>
2022-04-19 20:33:32 -0400

    Prevent faulty image API calls

--- src/actions/common-action.js
+++ src/actions/common-action.js
@@ -2,6 +2,12 @@ const API_BASE ="http://almost-somewhere.herokuapp.com";
 const PHOTOS_API = `${API_BASE}/places/photo`;
 
 export const findImage = (referenceId, maxH = 1000,maxW = 1000) => {
-    const imageSrc = `${PHOTOS_API}?photo_reference=${referenceId}&maxheight=${maxH}&maxwidth=${maxW}`;
-    return imageSrc;
+    if(referenceId.includes(".")){
+        return referenceId;
+    }else{
+        const imageSrc = `${PHOTOS_API}?photo_reference=${referenceId}&maxheight=${maxH}&maxwidth=${maxW}`;
+        return imageSrc;
+    }
+
+
 }
