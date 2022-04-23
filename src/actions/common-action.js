d20b8f19c9b0fc59f51aa881e96197e724dd8576
Shashwat <shashwatrathod22@gmail.com>
2022-04-23 14:35:30 -0400

    return placeholder in dev

--- src/actions/common-action.js
+++ src/actions/common-action.js
@@ -1,8 +1,12 @@
-import { REACT_APP_API_BASE } from "../config";
+import { NODE_ENV, REACT_APP_API_BASE } from "../config";
 
 const PHOTOS_API = `${REACT_APP_API_BASE}/places/photo`;
 
 export const findImage = (referenceId, maxH = 1000, maxW = 1000) => {
+  if (NODE_ENV !== "production") {
+    // Return placeholder image in development mode so that we dont get billed.
+    return "https://www.russorizio.com/wp-content/uploads/2016/07/ef3-placeholder-image.jpg";
+  }
   if (referenceId.includes(".")) {
     return referenceId;
   } else {
