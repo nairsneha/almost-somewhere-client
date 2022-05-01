c9d964b2affd0b042d6590f49ac93f297c6dab42
Shashwat <shashwatrathod22@gmail.com>
2022-05-01 11:46:49 -0400

    added known users reducer

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 // import './vendors/bootstrap/css/bootstrap.min.css';
 // import './vendors/bootstrap/js/bootstrap.bundle.min';
-import './vendors/bootstrap-theme/bootstrap.min.css';
-import AlmostSomewhere from './components';
-import HomeScreen from "./components/HomeScreen"
-import DetailsScreen from "./components/DetailsScreen"
+import "./vendors/bootstrap-theme/bootstrap.min.css";
+import AlmostSomewhere from "./components";
+import HomeScreen from "./components/HomeScreen";
+import DetailsScreen from "./components/DetailsScreen";
 import SignUp from "./components/SignupScreen/index";
-import Login from "./components/LoginScreen/index"
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Provider} from "react-redux";
-import {createStore, combineReducers} from "redux";
-import nearbyPlaceReducer  from "./reducers/nearby-place-reducer";
+import Login from "./components/LoginScreen/index";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import nearbyPlaceReducer from "./reducers/nearby-place-reducer";
 import placeDetailReducer from "./reducers/place-detail-reducer";
 import reviewsReducer from "./reducers/reviews-reducer";
 import userReducer from "./reducers/user-details-reducer";
 import ProfileScreen from "./components/ProfileScreen";
 import EditProfileScreen from "./components/EditProfileScreen";
-const reducer = combineReducers({nearByPlaces: nearbyPlaceReducer, placeDetail: placeDetailReducer, ourReviews: reviewsReducer,userStore:userReducer});
+import knownUsersReducer from "./reducers/known-users-reducer";
+const reducer = combineReducers({
+  nearByPlaces: nearbyPlaceReducer,
+  placeDetail: placeDetailReducer,
+  ourReviews: reviewsReducer,
+  userStore: userReducer,
+  knownUsers: knownUsersReducer,
+});
 const store = createStore(reducer);
 function App() {
-       return (
-           <Provider store={store}>
-         <BrowserRouter>
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AlmostSomewhere />}>
+            <Route index element={<HomeScreen />} />
 
-              <Routes>
-                  <Route path="/" element={<AlmostSomewhere />}>
+            <Route path="detail/:id" element={<DetailsScreen />} />
 
-                      <Route index element={<HomeScreen />} />
+            <Route path="profile">
+              <Route index element={<ProfileScreen />} />
+              <Route path=":username" element={<ProfileScreen />} />
+              <Route path="edit" element={<EditProfileScreen />} />
+            </Route>
+          </Route>
 
-                      <Route path="detail/:id" element={<DetailsScreen />} />
+          <Route path="/signup" element={<SignUp />} />
 
-                      <Route path="profile">
-                          <Route index element={<ProfileScreen />} />
-                          <Route path=":username" element={<ProfileScreen />} />
-                          <Route path="edit" element={<EditProfileScreen />} />
-                      </Route>
-                  </Route>
-
-                  <Route path="/signup"
-                         element={<SignUp/>} />
-
-                  <Route path="/login"
-                         element={<Login/>} />
-
-              </Routes>
-         </BrowserRouter>
-           </Provider>
-       );
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
-      
+
 export default App;
--- src/components/LoginScreen/index.js
+++ src/components/LoginScreen/index.js
@@ -1,88 +1,99 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {Link} from "react-router-dom";
-import api from '../../services/user-service'
-const Login=()=>{
-    const navigate = useNavigate();
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import api from "../../services/user-service";
+const Login = () => {
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch();
-    const[user, setUser]=useState({
-        username:null,
-        password:null
-      });
+  const dispatch = useDispatch();
+  const [user, setUser] = useState({
+    username: null,
+    password: null,
+  });
 
-    function isObjectEmpty(obj) {
-        for (var key in obj) {
-            if (obj[key] === null || obj[key] === "")
-                return true;
+  function isObjectEmpty(obj) {
+    for (var key in obj) {
+      if (obj[key] === null || obj[key] === "") return true;
+    }
+    return false;
+  }
+
+  const loginUser = () => {
+    if (isObjectEmpty(user)) {
+      alert("Enter all feilds");
+    } else {
+      const userDetails = api.loginUserSvc(user).then((data1) => {
+        if (data1 !== undefined) {
+          alert("Login successful");
+          dispatch({
+            type: "create-user",
+            user: data1.response,
+          });
+          navigate("/");
         }
-        return false;
+      });
     }
+  };
 
-    const loginUser =() =>{
-
-        if(isObjectEmpty(user)){
-            alert('Enter all feilds');
-        }else{
-
-           const userDetails= api.loginUserSvc(user)
-               .then((data1)=>{
-                   if(data1!==undefined){
-                       alert('Login successful');
-                       dispatch({
-                                    type: 'create-user',
-                                    user: data1.response
-                                });
-                       navigate('/');
-                   }
-               })
-
+  return (
+    <>
+      <br />
+      <div className="container">
+        <h1>Login</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">username</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Enter username"
+              name="username"
+              onChange={(e) =>
+                setUser({
+                  ...user,
+                  username: e.target.value,
+                })
               }
-
-    }
-
-
-    return (
-        <>
-            <br />
-            <div className="container">
-            <h1>Login
-            </h1>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="username">username</label>
-                    <input type="email" className="form-control" id="email"
-                           aria-describedby="emailHelp" placeholder="Enter username"
-                           name="username" onChange={(e)=>setUser({
-                                                                      ...user,
-                                                                      username:e.target.value
-                                                                  })}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password"
-                           placeholder="Password" name="password" onChange={(e)=>setUser({
-                                                                                             ...user,
-                                                                                             password:e.target.value
-                                                                                         })}/>
-                </div>
-<center>
-
-
-                <button type="button" className="btn btn-primary" onClick={(e)=>loginUser()}
-                style={{display:'inline-block'}}>Submit</button>
-    &nbsp;&nbsp;<Link to='/signup' style={{display:'inline-block'}}>
-                    <p> Register as  User?</p>
-                </Link>
-</center>
-            </form>
-            </div>
-        </>
-
-    );
-
-}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+              name="password"
+              onChange={(e) =>
+                setUser({
+                  ...user,
+                  password: e.target.value,
+                })
+              }
+            />
+          </div>
+          <center>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={(e) => loginUser()}
+              style={{ display: "inline-block" }}
+            >
+              Submit
+            </button>
+            &nbsp;&nbsp;
+            <Link to="/signup" style={{ display: "inline-block" }}>
+              <p> Register as User?</p>
+            </Link>
+          </center>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Login;
