28e05d8e665922b7da8aab9394f30e4d67ee361a
shethmanan <shethmanan1997@gmail.com>
2022-05-01 15:48:53 -0400

    Dashboard UI fix

--- src/components/HomeScreen/components/card-carousel-list.js
+++ src/components/HomeScreen/components/card-carousel-list.js
@@ -4,7 +4,7 @@ import 'react-multi-carousel/lib/styles.css';
 import Card from "./location-card";
 
 const CardCarouselList = ({
-                              title="Title",locDetails = [
+                              title="title",locDetails = [
                                   {
                                       name: "Web Development",
                                       photos: [{photo_reference: 'Aap_uEAiPnzpgDWC_ErLs0eTy-LF8_cCEidmA_ls-3B7o6-832qJgOAv_6UeoJ37j-eC6f20yJwC5SXOROwSxY4m16c59d5iMSAJZ0_s1psMfPNaCGWX1m2t4kx7Tcp2B4dCl2Oy_TooFjbV9P5poUXHkd7OT56ifckvECx7nkRMK0OhfCXS'}
@@ -33,11 +33,14 @@ const CardCarouselList = ({
             items: 1
         }
     };
+    const headerCase = (title) => {
+        return title.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+    }
     let filteredLocation = locDetails.filter((val) => val.photos && val.photos.length > 0);
     return (
         <>
             <div>
-                <span className="fs-2 fw-light">{title}</span>
+                <span className="fs-2 fw-light">{headerCase(title)}</span>
             </div>
             <Carousel responsive={responsive} autoPlaySpeed={3000} infinite itemClass="carousel-item-padding-40-px">
                 {filteredLocation.map((item) => {
