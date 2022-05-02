c82fa3ad4153932aafcb897769ce481f50567f18
nairsneha <nair.sneha1997@gmail.com>
2022-05-01 22:57:36 -0400

    added geolocation service

--- src/services/nearby-places-service.js
+++ src/services/nearby-places-service.js
@@ -6,11 +6,20 @@ const SEARCH_PLACE_API = `${REACT_APP_API_BASE}/places/search`;
 
 export const findNearbyPlace = async (params) => {
   const defaultParams = {
-    lat: -71.085637,
-    lon: 42.3417141,
+    lat: -71.0878681,
+    lon: 42.3380983,
     radius: 5000,
     type: "gym|theatre",
   };
+
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+      defaultParams.lon = position.coords.latitude;
+      defaultParams.lat = position.coords.longitude;
+    });
+  }
+
+
   const safeParams = { ...defaultParams, ...params };
   const response = await axios.get(
     `${NEARBY_PLACES_API}?latitude=${safeParams.lat}&longitude=${safeParams.lon}&radius=${safeParams.radius}&type=${safeParams.type}`
